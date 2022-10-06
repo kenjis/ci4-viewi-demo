5461a243d32455aaf45edf5f9dba64c87d9d7c19
kenjis <kenji.uui@gmail.com>
2022-10-06 14:44:31 +0900

    style: composer cs-fix

--- app/ViewiApp/config.php
+++ app/ViewiApp/config.php
@@ -3,13 +3,13 @@
 use Viewi\PageEngine;
 
 const VIEWI_COMPONENTS = __DIR__ . '/Components';
-const PUBLIC_FOLDER = __DIR__ . '/../../public/';
+const PUBLIC_FOLDER    = __DIR__ . '/../../public/';
 
 return [
-    PageEngine::SOURCE_DIR =>  VIEWI_COMPONENTS,
-    PageEngine::SERVER_BUILD_DIR =>  __DIR__ . '/build',
-    PageEngine::PUBLIC_ROOT_DIR => PUBLIC_FOLDER,
-    PageEngine::DEV_MODE => true,
-    PageEngine::RETURN_OUTPUT => true,
-    PageEngine::COMBINE_JS => true
+    PageEngine::SOURCE_DIR       => VIEWI_COMPONENTS,
+    PageEngine::SERVER_BUILD_DIR => __DIR__ . '/build',
+    PageEngine::PUBLIC_ROOT_DIR  => PUBLIC_FOLDER,
+    PageEngine::DEV_MODE         => true,
+    PageEngine::RETURN_OUTPUT    => true,
+    PageEngine::COMBINE_JS       => true,
 ];
