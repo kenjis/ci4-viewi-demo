6cc9d6d2a3e7f019da46374cac09fc5793bdd072
kenjis <kenji.uui@gmail.com>
2022-10-14 16:27:26 +0900

    config: remove 404 route from Viewi routes

--- app/ViewiApp/routes.php
+++ app/ViewiApp/routes.php
@@ -1,7 +1,6 @@
 <?php
 
 use Components\Views\Home\HomePage;
-use Components\Views\NotFound\NotFoundPage;
 use Components\Views\Pages\CounterPage;
 use Components\Views\Pages\TodoAppPage;
 use Viewi\Routing\Route as ViewiRoute;
@@ -9,4 +8,3 @@ use Viewi\Routing\Route as ViewiRoute;
 ViewiRoute::get('/', HomePage::class);
 ViewiRoute::get('/counter', CounterPage::class);
 ViewiRoute::get('/todo', TodoAppPage::class);
-ViewiRoute::get('*', NotFoundPage::class);
