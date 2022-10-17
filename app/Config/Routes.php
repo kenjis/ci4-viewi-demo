474f83bcb7a0e9a045d86022fe87d645c1867903
kenjis <kenji.uui@gmail.com>
2022-10-17 18:54:17 +0900

    style: break long lines and add empty lines

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -24,7 +24,6 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
